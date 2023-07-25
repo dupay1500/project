af446ec05ff9c5613033c4939aa7b01f7cf8f087
Mikky <mikky@gmail.com>
2023-07-25 16:36:25 +0500

    5-oy 7-dars kodi 1-4

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Article
+from .models import Article, Category
 
 # Create your views here.
 
 def index(request):
     articles = Article.objects.all()
+    # categories = Category.objects.all()
     context = {
         'articles': articles,
-        "title": "Maqolalar ro'yxati"
+        "title": "Maqolalar ro'yxati",
+        # 'categories': categories
     }
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/all_articles.html', context)
+
+
+def category_list(request, pk):
+    articles = Article.objects.filter(category_id=pk)
+    # categories = Category.objects.all()
+    context = {
+        'articles': articles,
+        # 'categories': categories
+    }
+    return render(request, "blog/all_articles.html", context)
