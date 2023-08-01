b8dc282395afb29fcf882b35c880a79cb89e1dbf
Mikky <mikky@gmail.com>
2023-08-01 16:55:30 +0500

    6-oy 1-dars 1-6

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('category/<int:pk>/', category_list, name='category_list'),
-    path('article/<int:pk>/', article_detail, name='article_detail'),
+    # path('article/<int:pk>/', article_detail, name='article_detail'),
+    # path('add/', add_article, name='lyuboy'),
+
+    path('', ArticleList.as_view(), name='index'),
+    path('article/<int:pk>/', ArticleDetail.as_view(), name='article_detail'),
+    path('new/', NewArticle.as_view(), name='lyuboy'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,19 +1,61 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView, DeleteView, DetailView, UpdateView, CreateView
+from django.urls import reverse_lazy, reverse
 
 from .models import Article, Category
+from .forms import ArticleForm
 
 # Create your views here.
 
-def index(request):
-    articles = Article.objects.filter(is_published=True)
-    # categories = Category.objects.all()
-    context = {
-        'articles': articles,
-        "title": "Maqolalar ro'yxati",
-        # 'categories': categories
+class ArticleList(ListView):
+    model = Article
+    context_object_name = 'articles'
+    # template_name = 'blog/all_articles.html'
+    extra_context = {
+        'title': "Maqolalar ro'yxati"
     }
-    return render(request, 'blog/all_articles.html', context)
+
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True)
+
+
+class ArticleDetail(DetailView):
+    model = Article
+
+    def get_queryset(self):
+        return Article.objects.filter(pk=self.kwargs['pk'], is_published=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = Article.objects.get(pk=self.kwargs['pk']).title
+        return context
+
+
+class NewArticle(CreateView):
+    form_class = ArticleForm
+    template_name = 'blog/article_form.html'
+    extra_context = {
+        'title': "class yordamnida ma'qola qo'shish"
+    }
+    success_url = reverse_lazy('index')
+
+
+
+
+
+
+
+
+# def index(request):
+#     articles = Article.objects.filter(is_published=True)
+#     # categories = Category.objects.all()
+#     context = {
+#         'articles': articles,
+#         "title": "Maqolalar ro'yxati",
+#         # 'categories': categories
+#     }
+#     return render(request, 'blog/all_articles.html', context)
 
 
 def category_list(request, pk):
@@ -26,10 +68,26 @@ def category_list(request, pk):
     return render(request, "blog/all_articles.html", context)
 
 
-def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk, is_published=True)
-    context = {
-        'title': "Maqola",
-        'article': article
-    }
-    return render(request, 'blog/detail.html', context)
+# def article_detail(request, pk):
+#     article = get_object_or_404(Article, pk=pk, is_published=True)
+#     context = {
+#         'title': "Maqola",
+#         'article': article
+#     }
+#     return render(request, 'blog/detail.html', context)
+
+
+# def add_article(request):
+#     if request.method == 'POST':
+#         form = ArticleForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             article = Article.objects.create(**form.cleaned_data)
+#             article.save()
+#             return redirect('article_detail', article.pk)
+#     else:
+#         form = ArticleForm()
+#     context = {
+#         'form': form,
+#         'title': "Maqola qo'shish"
+#     }
+#     return render(request, 'blog/article_form.html', context)
